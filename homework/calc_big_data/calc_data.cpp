f2bbcf0981f3deeb28c5a0349605fec1d835950c
FDws <885374150@qq.com>
2016-10-22 14:44:23 +0800

    fjkldsa

--- homework/calc_big_data/calc_data.cpp
+++ homework/calc_big_data/calc_data.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 struct point
 {
-	char sign_coefficient;
-	char sign_power;
+	string sign_coefficient;
+	string sign_power;
 	int* top_coefficient;
 	int* bottom_coefficient;
 	int* top_power;
 	int* bottom_power;
 	point* next;
 
-	point(char s1=' ',char s2=' ',string tc="1",string bc="1",string tp="1",string bp="1",point* p=NULL);
+	point(string s1="+",string s2="+",string tc="1",string bc="1",string tp="1",string bp="1",point* p=NULL);
 	void str_int(int id,string& st);
 };
-point::point(char s1,char s2,string tc,string bc,string tp,string bp,point* p)
+point::point(string s1,string s2,string tc,string bc,string tp,string bp,point* p)
 {
 	sign_coefficient=s1;
 	sign_power=s2;
@@ -52,7 +52,9 @@ class multi_data
 	public:
 		multi_data();
 		void input(string s);
-		void push(point* p);
+		void push(string sign_c,string sign_p,string s_ct,string s_cb,string s_pt,string s_pb);
+		void parse_str_data(string& t,string &s1,string &s2,string &s3,string &s4,string &s5,string &s6);
+		void parse_str_data_part(string &t,string &s1,string &s2,string& s3);
 		point* pop();
 		~multi_data();
 };
@@ -60,11 +62,49 @@ multi_data::multi_data()
 {
 	first = new point();
 }
-void multi_data::input(string s)
+void multi_data::parse_str_data_part(string &t,string &s1,string &s2,string& s3)
 {
+	if(t[0]=='-'||t[0]=='+')
+	{
+		s1=t[0];
+		t=t.substr(1);
+	}
 
-void multi_data::push(point* p)
+	unsigned long i=0;
+	while(t[i]!='/'&&i<t.length())
+	{
+		i++;
+	}
+
+	s2=t.substr(0,i);
+	if(i<t.length())
+	{
+		s3=t.substr(i+1);
+	}
+}
+
+void multi_data::parse_str_data(string& t,string &s1,string &s2,string &s3,string &s4,string &s5,string &s6)
 {
+	unsigned long i=0;
+	int l=0;
+	string t1;
+	string t2;
+
+	while(i<t.length())
+	{
+		if(t[i]==' ')
+		{
+			t1=t.substr(0,i);
+			t2=t.substr(i+3);
+			parse_str_data_part(t1,s1,s2,s3);
+			parse_str_data_part(t2,s4,s5,s6);
+			break;
+		}	
+	}
+}
+void multi_data::push(string sign_c,string sign_p,string s_ct,string s_cb,string s_pt,string s_pb)
+{
+	point* p = new point(sign_c,sign_p,s_ct,s_cb,s_pt,s_pb);
 	p->next=first->next;
 	first->next=p;
 }
@@ -72,9 +112,65 @@ point* multi_data::pop()
 {
 	point* t;
 	t=first->next;
-	first->next=t->next;
-	return t;
+
+	if(t)
+	{
+		first->next=t->next;
+		return t;
+	}
+	else 
+	{
+		return t;
+	}
 }
+void multi_data::input(string str)
+{
+	unsigned long i=0;
+	int l=0;
+	int b=0;
+	string s1;
+	string s2;
+	string s3;
+	string s4;
+	string s5;
+	string s6;
+	string t;
+
+	while(i<str.length())
+	{
+		s1="+";
+		s2="1";
+		s3="1";
+		s4="+";
+		s5="1";
+		s6="1";
+		b=0;
+		l=i;
+
+		while(b!=3&&i<str.length())
+		{
+			if(str[i]==' ')
+			{
+				b++;
+			}
+			i++;
+		}
+
+		if (b==3)
+		{
+			t=str.substr(l,i-l-1);
+		}
+		else
+		{
+			t=str.substr(l,i-l);
+		}
+		
+		parse_str_data(t,s1,s2,s3,s4,s5,s6);
+
+		push(s1,s4,s2,s3,s5,s6);	
+	}
+}
+
 multi_data::~multi_data()
 {
 	point* t=first;
